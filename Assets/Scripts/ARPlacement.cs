b4932b757603638939722af57c3aacaa28182aad
AreniumIon <jeffreydickenson9@gmail.com>
2022-04-06 19:47:21 -0500

    AR build setup

---
+++ Assets/Scripts/ARPlacement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+    private GameObject spawnedObject;
+
+    private Pose PlacementPose;
+    [SerializeField] ARRaycastManager aRRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    private void Update() {
+        if (spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            ARPlaceObject();
+
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    void UpdatePlacementIndicator() {
+        if (spawnedObject == null && placementPoseIsValid) {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    void UpdatePlacementPose() {
+        Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(.5f, .5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid) {
+            PlacementPose = hits[0].pose;
+        }
+    }
+
+    void ARPlaceObject() {
+        spawnedObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+    }
+}
+
