6fb1f855c791f3e56070cff84eec0f156ac583fd
Dickenson <jdd170002@utdallas.edu>
2022-04-08 11:22:32 -0500

    Gem prefab and spinner

---
+++ Assets/Scripts/AR/GemSpinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpinner : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 30f;
+
+    void Update()
+    {
+        float spinAmount = spinSpeed / Time.deltaTime;
+        transform.Rotate(Vector3.up, spinAmount);
+    }
+}
