097650e5ee9ca732117c43d8d2f21c4013e18034
Dickenson <jdd170002@utdallas.edu>
2022-04-08 11:34:46 -0500

    Gem hover

--- Assets/Scripts/AR/GemSpinner.cs
+++ Assets/Scripts/AR/GemSpinner.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class GemSpinner : MonoBehaviour
 {
+    [SerializeField] Transform target;
+
     [SerializeField] float spinSpeed = 30f;
+    [SerializeField] float hoverOffset = .5f;
+    [SerializeField] float hoverTime = 2f;
+
+    float hoverTimer = 0f;
 
     void Update()
     {
+        Spin();
+        Hover();
+    }
+
+    void Spin()
+    {
         float spinAmount = spinSpeed / Time.deltaTime;
-        transform.Rotate(Vector3.up, spinAmount);
+        target.Rotate(Vector3.up, spinAmount);
+    }
+
+    void Hover()
+    {
+        float sin = Mathf.Sin(hoverTimer / hoverTime);
+        float y = Mathf.Lerp(-hoverOffset, hoverOffset, sin);
+
+        target.transform.position = new Vector3(target.position.x, y, target.position.z);
     }
 }
