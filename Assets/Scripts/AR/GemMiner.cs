9e60ed672707b1658514ff967f6c34b1a753a3a5
AreniumIon <jeffreydickenson9@gmail.com>
2022-04-13 17:36:06 -0500

    Gem tapping particle system progress

---
+++ Assets/Scripts/AR/GemMiner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class GemMiner : MonoBehaviour
+{
+    [SerializeField] GameObject mineParticleSystemPrefab;
+
+    private void OnEnable()
+    {
+        ARInputManager.OnTouch += TryMine;
+    }
+
+    private void OnDisable()
+    {
+        ARInputManager.OnTouch -= TryMine;
+    }
+
+    private void TryMine(Touch touch)
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+        Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 100f));
+        Ray ray = new Ray(cameraPos, touchPos - cameraPos);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, ~0, QueryTriggerInteraction.Collide))
+        {
+            Instantiate(mineParticleSystemPrefab, hitInfo.point, Quaternion.identity, transform);
+        }
+    }
+}
---
+++ Assets/Scripts/AR/MineParticleSystemManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineParticleSystemManager : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (!ps.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
