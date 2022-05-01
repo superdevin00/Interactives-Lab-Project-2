bd3199c8c202357a4ef71929868a5cad3e81a4a2
AreniumIon <jeffreydickenson9@gmail.com>
2022-05-01 16:02:22 -0500

    Gem placing delay

--- Assets/Scripts/AR/ARPlacement.cs
+++ Assets/Scripts/AR/ARPlacement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using System;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.Android;
 
 public class ARPlacement : MonoBehaviour
 {
+    static float SPAWN_GEM_TIME_DELAY = .4f;
     static Vector3 STARTING_GEM_OFFSET_FROM_CAMERA = new Vector3(0f, 0f, 5f);
 
     [SerializeField] ARRaycastManager aRRaycastManager;
@@ -19,19 +21,26 @@ public class ARPlacement : MonoBehaviour
 
     private void Start()
     {
-        Vector3 cameraPos = (Camera.current) ? Camera.current.transform.position : new Vector3();
-        Pose startPose = new Pose(cameraPos + STARTING_GEM_OFFSET_FROM_CAMERA, Quaternion.identity);
-        ARPlaceObject(startPose);
+        StartCoroutine(WaitToPlaceObject());
     }
 
     private void Update()
     {
-        if (!hasSnappedToARObject)
+        if (spawnedObject != null && !hasSnappedToARObject)
             TrySnapToARObject();
     }
 
-    void ARPlaceObject(Pose pose)
+    IEnumerator WaitToPlaceObject()
     {
+        // Small delay because Android device doesn't enable camera immediately
+        yield return new WaitForSeconds(SPAWN_GEM_TIME_DELAY);
+        ARPlaceObject();
+    }
+
+    void ARPlaceObject()
+    {
+        Vector3 cameraPos = (Camera.current) ? Camera.current.transform.position : new Vector3();
+        Pose pose = new Pose(cameraPos + STARTING_GEM_OFFSET_FROM_CAMERA, Quaternion.identity);
         spawnedObject = Instantiate(arObjectToSpawn, pose.position, pose.rotation);
     }
 
