f72124e1889fb3e4f90f3d517a6c282a4a9ece7f
Devin <superdevin00@gmail.com>
2022-05-02 19:40:24 -0500

    Main Menu Play Button

---
+++ Assets/Scripts/UI/MainMenuScripts.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScripts : MonoBehaviour
+{
+    
+    public void loadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
