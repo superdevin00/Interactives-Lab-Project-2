8509cf9569772bbea33b78200ca26ca89057109c
Devin <superdevin00@gmail.com>
2022-05-04 15:43:32 -0500

    Mainmenu w/ highscore

--- Assets/Scripts/UI/MainMenuScripts.cs
+++ Assets/Scripts/UI/MainMenuScripts.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuScripts : MonoBehaviour
 {
-    
+    [SerializeField] TMP_Text highScoreText;
+    private void Start()
+    {
+        highScoreText.text = "Highscore:  " + PlayerPrefs.GetInt("highScore").ToString("0");
+    }
+
     public void LoadGameSetupScene()
     {
         SceneManager.LoadScene("GameSetupScene");
