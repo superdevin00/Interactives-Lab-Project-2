828464fb3e25591a0e8a6f60b686e94e974e969b
Devin <superdevin00@gmail.com>
2022-05-05 00:22:28 -0500

    Added sfx and music prefab

---
+++ Assets/Scripts/MusicSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSingleton : MonoBehaviour
+{
+    public static MusicSingleton i;
+    void Awake()
+    {
+        if (i == null)
+        {
+            //Retain on Load of New Scene
+            i = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
