f8ffc86d79569737d7f724c20a0f986123feea1b
Jacque Keener <87202026+JacqueKeener@users.noreply.github.com>
2022-05-04 23:58:13 -0500

    GPSTest set but fade in
    adding more soon

---
+++ Assets/Scripts/UI/FadeIn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer spr;
+    Color col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = spr.color;
+        col.a = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        col.a += Time.deltaTime;
+        spr.color = col;
+    }
+}
